6b52c398a0525f7a3137cdaf0f46c18f4a7e13d5
Otsuki Hitoshi <hitochan777@gmail.com>
2021-04-22 20:33:53 +0900

    Add next-seo

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,10 +1,12 @@
 import '../styles/global.css'
 import 'katex/dist/katex.css'
+import { DefaultSeo } from 'next-seo'
 import Footer from '../components/footer'
 
 export default function MyApp({ Component, pageProps }) {
   return (
     <>
+      <DefaultSeo />
       <Component {...pageProps} />
       <Footer />
     </>
