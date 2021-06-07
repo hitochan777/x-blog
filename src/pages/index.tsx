a6d981dc85ad767fedb087c130f92e24c86f8b2f
Otsuki Hitoshi <hitochan777@gmail.com>
2021-06-08 00:57:50 +0900

    Fix broken layout on smartphone

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -4,8 +4,8 @@ export default function Index() {
 	return (
 		<>
 			<Header titlePre="Home" />
-			<div className="myname" style={{transform: "translateY(calc(-1 * (64px + 2rem) / 2))"}}>
-				<h1 style={{fontSize: "5rem", margin: 0}}>hitochan777</h1>
+			<div style={{transform: "translateY(calc(-1 * (64px + 2rem) / 2))"}}>
+				<h1 className="myname">hitochan777</h1>
 				<div className="explanation">
 					Hi😺 My name is hitochan777 and I am a software developer who loves
 					React, GraphQL, Python and keen on learning new things.
