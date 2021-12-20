63370fe62e35ba7559f4316c18ad1f7ae765252d
Otsuki Hitoshi <hitochan777@gmail.com>
2021-12-20 23:00:50 +0900

    Add zenn link

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -13,6 +13,7 @@ export default function Index() {
         <div className="social-media" style={{ marginTop: '2rem' }}>
           <a href="https://github.com/hitochan777">GitHub</a>
           <a href="https://qiita.com/hitochan777">Qiita</a>
+          <a href="https://zenn.dev/hitochan777">Zenn</a>
         </div>
       </div>
     </>
